5c3f1a8f32dba768247d277eeba2093b3a7ecaa3
weitaiyun (Taiyun Wei) <weitaiyun@gmail.com>
2009-08-30 09:25:26 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/corrplot@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/corrplot/R/corrplot.mtest.R
+++ pkg/corrplot/R/corrplot.mtest.R
@@ -8,10 +8,28 @@ function(dat = NULL, corr = NULL, p.mat = NULL, conf.level = 0.95,
 		stop("need both corr and p.mat!")
 	if(plotConf&is.null(low.mat)&is.null(upp.mat)&is.null(dat))
 		stop("need low.mat and upp.mat!")
+		
+    cor.mtest <- function(mat, conf.level = NULL){
+	  mat <- as.matrix(mat)
+	  n <- ncol(mat)
+	  p.mat <- low.mat <- upp.mat <- matrix(NA, n, n)    
+	  diag(p.mat) <- 0
+	  diag(low.mat) <- diag(upp.mat) <- 1
+	  for(i in 1:(n-1)){
+		  for(j in (i+1):n){
+			  tmp <- cor.test(mat[,i], mat[,j], conf.level = conf.level)
+			  p.mat[i,j] <- p.mat[j,i] <- tmp$p.value
+			  low.mat[i,j] <- low.mat[j,i] <- tmp$conf.int[1]
+			  upp.mat[i,j] <- upp.mat[j,i] <- tmp$conf.int[2]
+		  }
+	  }
+	  return(list(p.mat, low.mat, upp.mat))
+    }
+	
 	if(!is.null(dat)){
 		if(!is.null(c(corr, p.mat, low.mat, upp.mat)))
 			warning("dat is specialized, so corr, p.mat, low.mat, upp.mat will be omitted!\n")
-		mcor <- cor.mtest(mat = dat,  conf.level = conf.level, method = "pearson")
+		mcor <- cor.mtest(mat = dat,  conf.level = conf.level)
 		corr <- cor(dat, use = "pair")
 		p.mat <- mcor[[1]]
 		low.mat <- mcor[[2]]
