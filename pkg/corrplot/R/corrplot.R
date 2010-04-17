ab8c578404b856ecd857f159d5791f17b2ccd9c4
weitaiyun (Taiyun Wei) <weitaiyun@gmail.com>
2010-04-17 01:21:09 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/corrplot@27 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/corrplot/R/corrplot.R
+++ pkg/corrplot/R/corrplot.R
@@ -28,7 +28,7 @@ corrplot <- function(corr, method = c("circle", "square", "ellipse", "number",
 		rect.hc = NA, rect.col="black", rect.lwd = 2,
 
 		col = colorRampPalette(c("#67001F", "#B2182B", "#D6604D", "#F4A582", 
-				"#FDDBC7", "#F7F7F7", "#D1E5F0", "#92C5DE", 
+				"#FDDBC7", "#FFFFFF", "#D1E5F0", "#92C5DE", 
 				"#4393C3", "#2166AC", "#053061"))(200),
              
 		outline = FALSE, cex = 1, title = "", bg = "white",
